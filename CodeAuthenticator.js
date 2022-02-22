4cfe96977d33a1a24382edb30afd20355161b0b8
adlDvlpr <99961161+adlDvlpr@users.noreply.github.com>
2022-02-22 21:14:12 +0100

    Create CodeAuthenticator.js

---
+++ CodeAuthenticator.js
@@ -0,0 +1,9 @@
+
+ var codeList= ["001", "002", "003", "004", "005"];
+ var code = prompt ("Type in your code");
+
+ if (codeList.includes(code)){
+    alert ("Welcome and Enjoy!");
+} else {
+    alert ("Sorry that code that not exist")
+}
